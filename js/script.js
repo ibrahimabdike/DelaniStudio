e85225673b94589ee6539f60700a3379fca52f08
Ibrahim Yahya <yahyalito1@gmail.com>
2020-05-14 11:01:35 +0300

    js functionality added

--- js/script.js
+++ js/script.js
@@ -16,3 +16,60 @@ function validateForm() {
     }
 
 }
+
+$(document).ready(function() {
+    $("#toClick1").click(function(){
+        $(".img1").toggle(100);
+        $(".design").toggle(100);
+    });
+    $("#toClick2").click(function(){
+        $(".img2").toggle(100);
+        $(".development").toggle(100);
+    });
+    $("#toClick3").click(function(){
+        $(".img3").toggle(100);
+        $(".pmanagement").toggle(100);
+    });
+
+    $("#work1")
+    .mouseenter(function () {
+        $("#text1").show("slow");
+    })
+    .mouseleave(function () {
+        $("#text1").hide("slow");
+    });
+
+    $("#work2")
+    .mouseenter(function () {
+        $("#text2").show("slow");
+    })
+    .mouseleave(function () {
+        $("#text2").hide("slow");
+    });
+
+    $("#work3")
+    .mouseenter(function () {
+        $("#text3").show("slow");
+    })
+    .mouseleave(function () {
+        $("#text3").hide("slow");
+    });
+
+    $("#work4")
+    .mouseenter(function () {
+        $("#text4").show("slow");
+    })
+    .mouseleave(function () {
+        $("#text4").hide("slow");
+    });
+
+    $("#work5")
+    .mouseenter(function () {
+        $("#text5").show("slow");
+    })
+    .mouseleave(function () {
+        $("#text5").hide("slow");
+    });
+
+
+})
